1bccea9dbd43d1abf6748587d274e42158a483b7
zehan <zehan9211@gmail.com>
2025-12-05 21:14:32 +0530

    chore: :art: Initial client configuration

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest/config" />
+
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/shared/config/test/setupTests.ts",
+  },
+});
